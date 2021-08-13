cd9cf97a841984b2aa99eeb53dd6878fdaff112a
MartinXPN <mirakyanmartin@gmail.com>
2021-08-13 13:21:08 +0400

    Added firebase to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import { initializeApp } from 'firebase/app';
+
 import './App.css';
 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCoZzYEGYmfNgEwVN2thZcAmQ7NBqnzlBQ",
+    authDomain: "profound-academy.firebaseapp.com",
+    projectId: "profound-academy",
+    storageBucket: "profound-academy.appspot.com",
+    messagingSenderId: "344681371914",
+    appId: "1:344681371914:web:1f4967125a364c0c4e42f3",
+    measurementId: "G-5EM92EE4GL"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log(firebaseApp);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;

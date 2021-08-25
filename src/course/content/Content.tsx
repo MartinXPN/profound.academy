6c16516eecfb03189f2a672ca393e12675694ff6
MartinXPN <mirakyanmartin@gmail.com>
2021-08-25 13:43:45 +0400

    lazy load prismjs languages

--- src/course/content/Content.tsx
+++ src/course/content/Content.tsx
@@ -4,8 +4,9 @@ import 'firebase/functions';
 
 import {ExtendedRecordMap} from "notion-types/src/maps";
 import {Code, Collection, CollectionRow, Equation, Modal, NotionRenderer} from 'react-notion-x'
-import 'react-notion-x/src/styles.css';     // core styles shared by all of react-notion-x (required)
+import { highlightAll } from "prismjs";
 import 'prismjs/themes/prism.css';          // used for code syntax highlighting (optional)
+import 'react-notion-x/src/styles.css';     // core styles shared by all of react-notion-x (required)
 import 'rc-dropdown/assets/index.css';      // used for collection views (optional)
 import 'katex/dist/katex.min.css';          // used for rendering equations (optional)
 import useAsyncEffect from "use-async-effect";
@@ -26,9 +27,14 @@ function Content(props: ContentProps) {
         setRecordMap(null);
         const getPage = firebase.functions().httpsCallable('getNotionPage');
         const map = await getPage({pageId: notionPage});
-        // console.log({map: map.data});
+        console.log({map: map.data});
         // @ts-ignore
         setRecordMap(map.data);
+
+        // TODO: Get the languages from record map obtained from getPage()
+        const languages = ['cpp', 'python', 'c'];
+        await Promise.all(languages.map(l => import(`prismjs/components/prism-${l}.min`)));
+        highlightAll();
     }, [notionPage]);
 
     return (

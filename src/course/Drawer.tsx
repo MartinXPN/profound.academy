f1f5c00501fc997f93f8824460c6aef3acc865b8
MartinXPN <mirakyanmartin@gmail.com>
2021-11-12 21:23:07 +0400

    do not show levels for a single-level courses

--- src/course/Drawer.tsx
+++ src/course/Drawer.tsx
@@ -109,7 +109,8 @@ function LevelList({levelNumber, exercises, progress, onItemSelected, isDrawerOp
                     isDrawerOpen: boolean}) {
     const {exerciseId} = useParams<{ exerciseId?: string }>();
     const isExerciseInLevel = exercises.filter(e => e.id === exerciseId).length > 0;
-    const [open, setOpen] = useState(isExerciseInLevel);
+    const isSingleLevel = exercises.length <= 1 || exercises.map((e, i) => i === 0 ? 0 : Math.floor(e.order) - Math.floor(exercises[i-1].order)).some(x => x > 0);
+    const [open, setOpen] = useState(isExerciseInLevel || isSingleLevel);
     const statusToStyle = useStatusToStyledBackground();
 
     useEffect(() => {
@@ -138,14 +139,16 @@ function LevelList({levelNumber, exercises, progress, onItemSelected, isDrawerOp
 
     return <>
         <List disablePadding>
-            <ListItem button onClick={onLevelClicked} className={levelClass}>
-                <ListItemIcon>
-                    <Equalizer />
-                    {!isDrawerOpen && <Typography variant="subtitle1">{levelNumber}</Typography>}
-                    {isDrawerOpen && <Typography variant="subtitle1">Level {levelNumber}</Typography>}
-                    {open ? <ArrowDropUp /> : <ArrowDropDown />}
-                </ListItemIcon>
-            </ListItem>
+            {!isSingleLevel &&
+                <ListItem button onClick={onLevelClicked} className={levelClass}>
+                    <ListItemIcon>
+                        <Equalizer/>
+                        {!isDrawerOpen && <Typography variant="subtitle1">{levelNumber}</Typography>}
+                        {isDrawerOpen && <Typography variant="subtitle1">Level {levelNumber}</Typography>}
+                        {open ? <ArrowDropUp/> : <ArrowDropDown/>}
+                    </ListItemIcon>
+                </ListItem>
+            }
             {open && exercises.map((ex, index) =>
                 <ListItem button key={ex.id} onClick={() => onItemSelected(ex.id)} className={getStatusStyle(ex.id)}>
                     <ListItemIcon>{exerciseId === ex.id ? <ArrowRightIcon /> : <ListItemText primary={index + 1}/>}</ListItemIcon>

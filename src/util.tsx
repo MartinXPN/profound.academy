1f23d12eb1cc46b57eafec6d893251f5774135a3
MartinXPN <mirakyanmartin@gmail.com>
2021-11-27 17:23:46 +0400

    implemented pagination for submission table

--- src/util.tsx
+++ src/util.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {MutableRefObject, useEffect, useState} from "react";
 
 
 
@@ -36,4 +36,22 @@ export function useStickyState<T>(defaultValue: T, key: string) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value]);
     return [value, setValue];
+}
+
+
+export function useOnScreen(ref: MutableRefObject<any>) {
+
+    const [isIntersecting, setIntersecting] = useState(false);
+    const observer = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting));
+
+    useEffect(() => {
+        if( ref.current )
+            observer.observe(ref.current);
+        // Remove the observer as soon as the component is unmounted
+        return () => { observer.disconnect() }
+        // intentionally not include observer as it's created on every render
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ref]);
+
+    return isIntersecting;
 }

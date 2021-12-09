fd6507d09ad1fcfb4ca8cdbf979c850078ce41a5
MartinXPN <mirakyanmartin@gmail.com>
2021-12-09 12:16:37 +0400

    added python3 language code

--- functions/src/models/language.ts
+++ functions/src/models/language.ts
@@ -4,11 +4,12 @@ export interface Language {
     displayName: string;
 }
 
-export const LANGUAGES: {[key: string]: Language} = {
+export const LANGUAGES: { [key: string]: Language } = {
     'C': {extension: 'c', languageCode: 'C', displayName: 'C'},
     'C++11': {extension: 'cpp', languageCode: 'C++11', displayName: 'C++11'},
     'C++14': {extension: 'cpp', languageCode: 'C++14', displayName: 'C++14'},
     'C++17': {extension: 'cpp', languageCode: 'C++17', displayName: 'C++17'},
     'C++20': {extension: 'cpp', languageCode: 'C++20', displayName: 'C++20'},
     'python': {extension: 'py', languageCode: 'python', displayName: 'Python'},
+    'python3': {extension: 'py', languageCode: 'python3', displayName: 'Python 3'},
 };

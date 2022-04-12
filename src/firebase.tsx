928c880c65e79736335510683188f37e2e9a75e2
MartinXPN <mirakyanmartin@gmail.com>
2022-04-12 10:43:15 +0400

    added Firebase test app config

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -6,17 +6,28 @@ import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 // import 'firebase/functions';
 // import 'firebase/storage';
 
-export const firebaseConfig = {
+const testConfig = {
+    apiKey: "AIzaSyC0zY64-QLVwj2TdoPxtvcU02kwhrM_Eww",
+    authDomain: "profound.academy",
+    projectId: "profound-academy-test",
+    storageBucket: "profound-academy-test.appspot.com",
+    messagingSenderId: "793395170665",
+    appId: "1:793395170665:web:7e7bbf06165e3116a0c1f8",
+    measurementId: "G-XK541903MD",
+}
+
+const prodConfig = {
     apiKey: "AIzaSyCoZzYEGYmfNgEwVN2thZcAmQ7NBqnzlBQ",
     authDomain: "profound.academy",
     projectId: "profound-academy",
     storageBucket: "profound-academy.appspot.com",
     messagingSenderId: "344681371914",
     appId: "1:344681371914:web:1f4967125a364c0c4e42f3",
-    measurementId: "G-5EM92EE4GL"
-};
+    measurementId: "G-5EM92EE4GL",
+}
 
-const app = firebase.initializeApp(firebaseConfig);
+
+const app = firebase.initializeApp(prodConfig);
 const appCheck = initializeAppCheck(app, {
     provider: new ReCaptchaV3Provider('6LcpsDAfAAAAAO04Nn88Eu88h7DldZGFu-OUrEN1'),
     // Optional argument. If true, the SDK automatically refreshes App Check tokens as needed.

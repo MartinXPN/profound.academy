c67841ca56a5f1d5a8f354ce3cf0bca66b1a501f
MartinXPN <mirakyanmartin@gmail.com>
2022-05-17 10:25:55 +0400

    Added sendInviteEmails test and fixed diffEmails bug

--- functions/test/courses.test.ts
+++ functions/test/courses.test.ts
@@ -2,6 +2,7 @@ import * as admin from 'firebase-admin';
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import {config} from "./testConfig";
+import {firestore} from 'firebase-admin';
 
 const assert = chai.assert;
 
@@ -65,36 +66,56 @@ describe('Handle Course Instructor Permissions', function () {
 });
 
 
-// describe('Send Course Invite Emails', function () {
-//     // Firebase connection can take long to be established
-//     this.timeout(5000);
-//
-//     let courses: typeof import('../src/services/courses');
-//     let db: typeof import('../src/services/db').db;
-//     let adminInitStub: sinon.SinonStub;
-//     let firestoreStub: sinon.SinonStub;
-//     let courseId: string;
-//
-//     beforeEach(async () => {
-//         const app = admin.apps.length === 0 ? admin.initializeApp(config) : admin.apps[0] ?? undefined;
-//         adminInitStub = sinon.stub(admin, 'initializeApp');
-//         firestoreStub = sinon.stub(admin, 'firestore').callsFake(() => admin.firestore(app));
-//         courses = await import('../src/services/courses');
-//         db = (await import('../src/services/db')).db;
-//         courseId = db.courses.doc().id;
-//     });
-//
-//     afterEach(async () => {
-//         firestoreStub.restore();
-//         adminInitStub.restore();
-//         await admin.firestore().recursiveDelete(db.course(courseId));
-//     });
-//
-//     describe('Send invitation', () => {
-//         it('Check if the invitation field is populated properly', async () => {
-//             // TODO: Populate invite emails and send invites
-//             // TODO: send invite to the same person twice - it should ignore the second invitation
-//             // assert.equal(exercise2Overall?.runs, 1, 'Exercise 2 should have only 1 value saved');
-//         });
-//     });
-// });
+describe('Send Course Invite Emails', function () {
+    // Firebase connection can take long to be established
+    this.timeout(5000);
+
+    let courses: typeof import('../src/services/courses');
+    let db: typeof import('../src/services/db').db;
+    let adminInitStub: sinon.SinonStub;
+    let firestoreStub: sinon.SinonStub;
+    let courseId: string;
+
+    beforeEach(async () => {
+        const app = admin.apps.length === 0 ? admin.initializeApp(config) : admin.apps[0] ?? undefined;
+        adminInitStub = sinon.stub(admin, 'initializeApp');
+        firestoreStub = sinon.stub(admin, 'firestore').callsFake(() => admin.firestore(app));
+        courses = await import('../src/services/courses');
+        db = (await import('../src/services/db')).db;
+        courseId = db.courses.doc().id;
+    });
+
+    afterEach(async () => {
+        firestoreStub.restore();
+        adminInitStub.restore();
+        await admin.firestore().recursiveDelete(db.course(courseId));
+        await admin.firestore().recursiveDelete(db.mails);
+    });
+
+    describe('Send invitation', () => {
+        it('Check if the invitation field is populated properly', async () => {
+            const emails = ['abc@gmail.com', 'cba@gmail.com', 'hey@profound'];
+            await db.coursePrivateFields(courseId).set({
+                id: courseId,
+                invitedEmails: emails,
+                mailSubject: 'Invitation!!!',
+                mailText: 'You have been invited! Follow the link to participate',
+            });
+
+            await courses.sendInviteEmails(courseId);
+            let invitationMails = (await db.mails.get()).docs.map(d => d.data());
+            assert.equal(invitationMails.length, 3, 'We should send 3 invitation emails');
+
+            // Send another invitation
+            await db.coursePrivateFields(courseId).update({
+                invitedEmails: firestore.FieldValue.arrayUnion('another@gmail.com'),
+            });
+            await courses.sendInviteEmails(courseId);
+            invitationMails = (await db.mails.get()).docs.map(d => d.data());
+            assert.equal(invitationMails.length, 4, 'We should send another invitation');
+
+            const privateFields = (await db.coursePrivateFields(courseId).get()).data();
+            chai.expect(privateFields?.sentEmails ?? []).to.include.all.members([...emails, 'another@gmail.com']);
+        });
+    });
+});

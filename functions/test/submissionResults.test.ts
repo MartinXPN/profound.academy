77c979fc93fb45dcdc3606c44f18bb4b37ada6b3
MartinXPN <mirakyanmartin@gmail.com>
2022-05-19 23:57:30 +0400

    Fixed #169 and added tests for unlocking content

--- functions/test/submissionResults.test.ts
+++ functions/test/submissionResults.test.ts
@@ -169,11 +169,68 @@ describe('Process submission result', function () {
             } as JudgeResult;
 
             await submissionResults.processResult(judgeResult, userId, submissionDoc.id);
-            const userExerciseProgress = (await db.userProgress(courseId, userId).collection('exerciseUpsolveScore').doc('1').get()).data();
-            assert.equal(userExerciseProgress?.['progress']?.[exerciseId], 80, `Upsolve score should be updated: ${JSON.stringify(userExerciseProgress)}`);
+            const userUpsovingExerciseProgress = (await db.userProgress(courseId, userId).collection('exerciseUpsolveScore').doc('1').get()).data();
+            const userExerciseProgress = (await db.userProgress(courseId, userId).collection('exerciseScore').doc('1').get()).data();
+            assert.equal(userUpsovingExerciseProgress?.['progress']?.[exerciseId], 80, `Upsolve score should be updated: ${JSON.stringify(userUpsovingExerciseProgress)}`);
+            assert.equal(userExerciseProgress?.['progress']?.[exerciseId] ?? 0, 0, `Regular score should not be updated: ${JSON.stringify(userExerciseProgress)}`);
+
             const userProgress = (await db.userProgress(courseId, userId).get()).data();
             assert.equal(userProgress?.upsolveScore, 80, `Upsolve score should be updated for course: ${JSON.stringify(userProgress)}`);
             assert.equal(userProgress?.levelUpsolveScore?.['1'], 80, `Upsolve score should be updated for level: ${JSON.stringify(userProgress)}`);
+        });
+    });
+
+    describe('Unlock side quests', () => {
+        it('Should unlock a side quest', async () => {
+            const tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            const questDoc = await db.courses.add({
+                id: courseId,
+                img: 'https://i.imgur.com/Mcvgbvm.jpeg',
+                revealsAt: admin.firestore.FieldValue.serverTimestamp() as admin.firestore.Timestamp,
+                freezeAt: admin.firestore.Timestamp.fromDate(tomorrow),
+                visibility: 'private',
+                rankingVisibility: 'private',
+                allowViewingSolutions: false,
+                title: 'Test Side Quest',
+                author: 'Test Framework',
+                instructors: [],
+                introduction: '',
+                levelExercises: {'1': 4},
+                levelScores: {'1': 400},
+                exercises: [],
+            });
+            await db.exercise(courseId, exerciseId).update({unlockContent: [questDoc.id]});
+
+            const creationDate = new Date();
+            creationDate.setDate(creationDate.getDate() + 3);
+            const submission = {
+                id: '',
+                userId: userId,
+                course: db.course(courseId), exercise: db.exercise(courseId, exerciseId),
+                createdAt: admin.firestore.Timestamp.fromDate(creationDate),
+                code: {'main.py': 'print("hello hello")'},
+                language: 'python',
+                isTestRun: false,
+            };
+            const submissionDoc = await db.submissionQueue(userId).add(submission as unknown as Submission);
+            const judgeResult = {
+                overall: {status: 'Solved', score: 100, memory: 52, time: 0.1, returnCode: 0},
+                compileResult: {status: 'Solved', score: 0, memory: 100, time: 1, returnCode: 0},
+                testResults: [
+                    {status: 'Solved', score: 50, memory: 40, time: 0.1, returnCode: 0},
+                    {status: 'Solved', score: 50, memory: 52, time: 0.1, returnCode: 0},
+                ]
+            } as JudgeResult;
+
+            await submissionResults.processResult(judgeResult, userId, submissionDoc.id);
+            let userData = (await db.user(userId).get()).data();
+            let submissionResult = (await db.submissionResult(submissionDoc.id).get()).data();
+            chai.expect(submissionResult?.message).to.have.string('Congratulations!', 'Should congratulate for unlocking');
+            assert.isNotEmpty(userData?.courses?.filter((c) => (c.id === questDoc.id)), `The user should have the course now ${JSON.stringify(userData?.courses)}`);
+
+            const questPrivateFields = (await db.coursePrivateFields(questDoc.id).get()).data();
+            chai.expect(questPrivateFields?.invitedUsers ?? []).to.include(userId, `The user should be invited now ${JSON.stringify(questPrivateFields)}`);
         });
     });
 });

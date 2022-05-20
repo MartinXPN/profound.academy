de161f38cc2fe8ec4dc7f6337a1dfb025ac77b51
MartinXPN <mirakyanmartin@gmail.com>
2022-05-20 17:11:13 +0400

    Added a tooltip for hovering over exercises and levels

--- src/course/colors.ts
+++ src/course/colors.ts
@@ -2,7 +2,7 @@ import {SubmissionStatus} from "models/submissions";
 
 function adjust(color: string, amount: number) {
     return '#' + color.replace(/^#/, '')
-                      .replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+                      .replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).slice(-2));
 }
 
 export const statusColors = {

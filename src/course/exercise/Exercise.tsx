40ec8616601dfd3db9c8e44c1df4b8fcdf7ea96a
MartinXPN <mirakyanmartin@gmail.com>
2022-05-29 17:57:27 +0400

    Added conditional splitting and an additional Solve tab for narrow screens

--- src/course/exercise/Exercise.tsx
+++ src/course/exercise/Exercise.tsx
@@ -1,5 +1,5 @@
 import {useParams} from "react-router-dom";
-import React, {lazy, useCallback, useContext, useState, memo, Suspense, useEffect} from "react";
+import React, {lazy, useCallback, useContext, useState, memo, Suspense, useEffect, ReactChild} from "react";
 import {AuthContext} from "../../App";
 import {useStickyState} from "../../common/stickystate";
 import CourseLandingPage from "../CourseLandingPage";
@@ -22,23 +22,41 @@ import Checkboxes from "./Checkboxes";
 import MultipleChoice from "./MultipleChoice";
 import Dashboard from "./Dashboard";
 import {LocalizeContext} from "../../common/Localization";
+import useWindowDimensions from "../../common/windowDimensions";
 
 const ExerciseEditor = lazy(() => import('./ExerciseEditor'));
+
+function ConditionalSplitPane({split, children, defaultSizes, onDragFinished}: {
+    split: boolean,
+    defaultSizes?: number[],
+    onDragFinished?: (sizes: number[]) => void,
+    children: ReactChild[],
+}) {
+    if( !split )
+        return <>{children}</>
+    return <>
+        <SplitPane split="vertical" defaultSizes={defaultSizes} onDragFinished={onDragFinished}>
+            {children}
+        </SplitPane>
+    </>
+}
 
 
 function Exercise({launchCourse, registerCourse}: {launchCourse: () => void, registerCourse: () => void}) {
     const auth = useContext(AuthContext);
     const {course} = useContext(CourseContext);
     const {exercise} = useContext(CurrentExerciseContext);
+    const {width} = useWindowDimensions();
     console.log('exercise:', exercise);
 
     const {localize} = useContext(LocalizeContext);
     const {exerciseId} = useParams<{ exerciseId: string }>();
     const [exerciseType, setExerciseType] = useState<keyof typeof EXERCISE_TYPES>(exercise?.exerciseType ?? 'code');
     const [splitPos, setSplitPos] = useStickyState<number[] | null>(null, `splitPos-${auth?.currentUserId}`);
-    const [currentTab, setCurrentTab] = useState<'description' | 'allSubmissions' | 'bestSubmissions' | 'codeDrafts' | 'edit'>('description');
+    const [currentTab, setCurrentTab] = useState<'description' | 'solve' | 'allSubmissions' | 'bestSubmissions' | 'codeDrafts' | 'edit'>('description');
     const [codeDraftId, setCodeDraftId] = useState<string | null>(null);
     const isCourseInstructor = course && auth.currentUserId && course.instructors.includes(auth.currentUserId);
+    const split = Boolean(width > 800);
 
     useEffect(() => setExerciseType(exercise?.exerciseType ?? 'code'), [exercise]);
     useEffect(() => {
@@ -55,7 +73,19 @@ function Exercise({launchCourse, registerCourse}: {launchCourse: () => void, reg
     if( !course )
         return <></>
     let right = <></>;
-    if( exerciseType === 'code' ) {
+    if( !auth.isSignedIn ) {
+        right = <>
+            <Grid container direction="column" alignItems="center" justifyContent="center" height="100%">
+                <Stack direction="column" alignItems="center">
+                    <Typography variant="subtitle2" textAlign="center">
+                        To check your solution you need to sign in
+                    </Typography>
+                    <SignIn/>
+                </Stack>
+            </Grid>
+        </>
+    }
+    else if( exerciseType === 'code' ) {
         if( !!codeDraftId && currentTab === 'codeDrafts' )  right = <Editor disableCodeSync userId={codeDraftId} />
         else                                                right = <Editor />
     }
@@ -73,11 +103,11 @@ function Exercise({launchCourse, registerCourse}: {launchCourse: () => void, reg
 
         {/* Display the exercise of the course at the location where it was left off the last time*/}
         {exercise &&
-            <SplitPane split="vertical" defaultSizes={splitPos ?? [1, 1]} onDragFinished={onSplitChanged}>
+            <ConditionalSplitPane split={split} defaultSizes={splitPos ?? [1, 1]} onDragFinished={onSplitChanged}>
                 <Box width="100%" height="100%" sx={{overflowY: 'auto'}}>
-
                     <Grid container justifyContent="center">
                         <OutlinedButton selected={currentTab === 'description'} onClick={() => setCurrentTab('description')}>Description</OutlinedButton>
+                        {!split && <OutlinedButton selected={currentTab === 'solve'} onClick={() => setCurrentTab('solve')}>Solve</OutlinedButton>}
                         <OutlinedButton selected={currentTab === 'bestSubmissions'} onClick={() => setCurrentTab('bestSubmissions')}>Best Submissions</OutlinedButton>
                         <OutlinedButton selected={currentTab === 'allSubmissions'} onClick={() => setCurrentTab('allSubmissions')}>All Submissions</OutlinedButton>
                         {isCourseInstructor && <OutlinedButton selected={currentTab === 'codeDrafts'} onClick={() => setCurrentTab('codeDrafts')}>Code Drafts</OutlinedButton>}
@@ -85,24 +115,15 @@ function Exercise({launchCourse, registerCourse}: {launchCourse: () => void, reg
                     </Grid>
 
                     {currentTab === 'description' && <>{isCourseInstructor && <Dashboard/>}<Content notionPage={localize(exercise.pageId)}/>{auth.isSignedIn && <Forum/>}</>}
+                    {currentTab === 'solve' && <>{right}</>}
                     {currentTab === 'bestSubmissions' && <ExerciseSubmissionsTable rowsPerPage={20} course={course} exercise={exercise} mode="best" />}
                     {currentTab === 'allSubmissions' && <ExerciseSubmissionsTable rowsPerPage={20} course={course} exercise={exercise} mode="all" />}
                     {currentTab === 'codeDrafts' && <CodeDrafts onCodeDraftSelected={setCodeDraftId} />}
                     {currentTab === 'edit' && <Suspense fallback={<></>}><ExerciseEditor cancelEditing={() => setCurrentTab('description')} exerciseTypeChanged={setExerciseType} /></Suspense>}
                 </Box>
 
-                <Box width="100%" height="100%">
-                    {!auth.isSignedIn
-                        ? <Grid container direction="column" alignItems="center" justifyContent="center" height="100%">
-                            <Stack direction="column" alignItems="center">
-                                <Typography variant="subtitle2" textAlign="center">To check your solution you need to sign in</Typography>
-                                <SignIn />
-                            </Stack>
-                        </Grid>
-                        : <>{right}</>
-                    }
-                </Box>
-            </SplitPane>
+                {split ? <Box width="100%" height="100%">{right}</Box> : <></>}
+            </ConditionalSplitPane>
         }
     </>
 }

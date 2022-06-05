9fb6b22c1922706cc9d98d0cfc676a82c1e28764
MartinXPN <mirakyanmartin@gmail.com>
2022-06-05 19:42:55 +0400

    Refactored SubmissionsTable into a functional component

---
+++ src/course/submission/SubmissionsPage.tsx
@@ -0,0 +1,50 @@
+import React, {memo, useContext, useState} from "react";
+import {SubmissionResult} from "models/submissions";
+import useAsyncEffect from "use-async-effect";
+import SubmissionView from "./SubmissionView";
+import {Column} from "./SubmissionsTable";
+import {lastExerciseId} from "../Course";
+import {useNavigate} from "react-router-dom";
+import {AuthContext} from "../../App";
+
+function SubmissionsPage({startAfterId, loadSubmissions, columns, startIndex, onMoreLoaded}: {
+    startAfterId: string | null,
+    loadSubmissions: (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) => Promise<() => void>
+    columns: Column[],
+    startIndex: number,
+    onMoreLoaded: (lastId: string) => void,
+}) {
+    const navigate = useNavigate();
+    const auth = useContext(AuthContext);
+    const [submissions, setSubmissions] = useState<SubmissionResult[]>([]);
+
+    useAsyncEffect(async () => {
+        return await loadSubmissions(startAfterId, (submissions, more) => {
+            setSubmissions(submissions);
+            more && submissions.length > 0 && onMoreLoaded(submissions[submissions.length - 1].id);
+        });
+    }, unsubscribe => unsubscribe && unsubscribe(), [startAfterId]);
+
+    const onUserClicked = (userId: string) => navigate(`/users/${userId}`);
+    const onExerciseClicked = (courseId: string, exerciseId: string) => navigate(`/${courseId}/${exerciseId}`);
+    const onCourseClicked = (courseId: string) => {
+        const lastEx = lastExerciseId(auth.currentUserId, courseId);
+        if( lastEx )    navigate(`/${courseId}/${lastEx}`);
+        else            navigate(`/${courseId}`);
+    };
+
+    let orderNumber = startIndex;
+    return <>
+        {submissions.map((row, _index) =>
+            <SubmissionView
+                submission={row}
+                displayColumns={columns}
+                onCourseClicked={onCourseClicked}
+                onExerciseClicked={onExerciseClicked}
+                onUserClicked={onUserClicked}
+                orderNumber={orderNumber++} />
+        )}
+    </>
+}
+
+export default memo(SubmissionsPage);
--- src/course/submission/SubmissionsTable.tsx
+++ src/course/submission/SubmissionsTable.tsx
@@ -1,4 +1,4 @@
-import React, {Component, useContext, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -12,14 +12,8 @@ import {Exercise} from "models/exercise";
 import {onCourseSubmissionsChanged, onSubmissionsChanged, onUserExerciseSubmissionsChanged, onUserSubmissionsChanged} from "../../services/submissions";
 import {SubmissionResult} from "models/submissions";
 import moment from "moment/moment";
-import {lastExerciseId} from "../Course";
 import InfiniteScrollLoading from "../../common/InfiniteScrollLoading";
-import {Typography} from "@mui/material";
-import Box from "@mui/material/Box";
-import {useNavigate} from "react-router-dom";
-import {NavigateFunction} from "react-router";
-import {LocalizeContext} from "../../common/Localization";
-import SubmissionView from "./SubmissionView";
+import SubmissionsPage from "./SubmissionsPage";
 
 
 export interface Column {
@@ -42,178 +36,114 @@ const columns: Column[] = [
     { id: 'createdAt', label: 'Date', minWidth: 100, format: (value) => moment(value.toDate()).locale('en').format('YYYY MMM Do, HH:mm:ss') },
 ];
 
+function SubmissionsTable({reset, loadSubmissions, columns, rowsPerPage}: {
+    reset: number,
+    loadSubmissions: (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) => Promise<() => void>
+    columns: Column[],
+    rowsPerPage: number,
+}) {
+    const [hasMore, setHasMore] = useState(true);
+    const [lastId, setLastId] = useState<string | null>(null);
+    const [startAfterIds, setStartAfterIds] = useState<(string | null)[]>([]);
 
-interface Props {
-    navigate: NavigateFunction,
-    reset: number;
-    onLoadNext: (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) => Promise<() => void>;
-    columns: Column[];
-    localize: (text: string | {[key: string]: string}) => string,
-}
+    useEffect(() => {
+        setHasMore(true);
+        setLastId(null);
+        setStartAfterIds([]);
+    }, [reset]);
 
-interface State {
-    page: number;
-    hasMore: boolean;
-    pageSubmissions: SubmissionResult[][];
-    updateSubscriptions: (() => void)[],
-}
-
-class SubmissionsTable extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {page: 0, hasMore: true, pageSubmissions: [], updateSubscriptions: []};
+    const loadNextPage = () => {
+        setStartAfterIds(startAfterIds => {
+            if( startAfterIds.length > 0 && startAfterIds[startAfterIds.length - 1] === lastId )
+                return startAfterIds;
+            console.log('load next page...', startAfterIds);
+            setHasMore(false);
+            return [...startAfterIds, lastId];
+        });
     }
-
-    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {
-        if( prevProps.reset !== this.props.reset ) {
-            this.setState({page: 0, hasMore: true, pageSubmissions: [], updateSubscriptions: []});
+    const onMoreLoaded = (pageId: number, lastId: string) => {
+        console.log('onMoreLoaded:', pageId, lastId);
+        if(pageId === startAfterIds.length - 1) {
+            setLastId(lastId);
+            setTimeout(() => setHasMore(true), 100);    // Avoid loading two times in a raw
         }
     }
 
-    componentWillUnmount() {
-        this.unsubscribeAll();
-    }
-
-    onUserClicked = (userId: string) => this.props.navigate(`/users/${userId}`);
-    onExerciseClicked = (courseId: string, exerciseId: string) => this.props.navigate(`/${courseId}/${exerciseId}`);
-    onCourseClicked = (courseId: string) => {
-        const lastEx = lastExerciseId(this.context.auth?.currentUserId, courseId);
-        if( lastEx )    this.props.navigate(`/${courseId}/${lastEx}`);
-        else            this.props.navigate(`/${courseId}`);
-    };
-    unsubscribeAll = () => {
-        console.log('unsubscribing from all the submission listeners');
-        for( const unsubscribe of this.state.updateSubscriptions )
-            unsubscribe();
-    }
-
-
-    loadNextPage = async () => {
-        const page = this.state.page;
-        const startAfterId = page === 0 || !this.state.pageSubmissions[page - 1]
-            ? null
-            : this.state.pageSubmissions[page - 1].at(-1)?.id;
-
-        // do not load if we already have a listener and set dummy listener right after check
-        if( this.state.updateSubscriptions[page] )
-            return;
-        this.setState({updateSubscriptions: [...this.state.updateSubscriptions, () => {}]});
-
-        console.log('startAfter:', startAfterId, 'for page:', this.state.page);
-
-        const onChanged = (submissions: SubmissionResult[], more: boolean) => {
-            console.log('onChanged:', submissions, page, `(now ${this.state.page}) has more: ${more}`);
-            const currentSubscriptions = [...this.state.pageSubmissions];
-            currentSubscriptions[page] = submissions;
-            this.setState({pageSubmissions: currentSubscriptions});
-
-            if( page === this.state.page )
-                this.setState({hasMore: more, page: page + 1});
-        };
-
-        const unsubscribe = await this.props.onLoadNext(startAfterId ?? null, onChanged);
-        const subscriptions = [...this.state.updateSubscriptions];
-        subscriptions[page] = unsubscribe;
-        this.setState({updateSubscriptions: subscriptions});
-    };
-
-
-    render() {
-        const {hasMore, pageSubmissions} = this.state;
-        let orderNumber = 1;
-        console.log('page submissions:', pageSubmissions.length, 'hasMore:', hasMore);
-        if( (pageSubmissions.length === 0 || (pageSubmissions.length === 1 && pageSubmissions[0].length === 0)) && !hasMore )
-            return <Box alignItems="center" alignContent="center" textAlign="center">
-                <Typography>No submissions yet to display</Typography>
-            </Box>
-
-        return (
-            <Paper sx={{width: '100%'}}>
-                <TableContainer>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                {this.props.columns.map((column) => (
-                                    <TableCell key={column.id} align={column.align} sx={{ minWidth: column.minWidth }}>
-                                        {column.label}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {pageSubmissions.map((submissions, _page) => submissions.map((row, _index) =>
-                                <SubmissionView
-                                    submission={row}
-                                    displayColumns={this.props.columns}
-                                    onCourseClicked={this.onCourseClicked}
-                                    onExerciseClicked={this.onExerciseClicked}
-                                    onUserClicked={this.onUserClicked}
-                                    orderNumber={orderNumber++} />
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <InfiniteScrollLoading hasMore={hasMore} loadMore={this.loadNextPage} />
-            </Paper>
-        );
-    }
+    return <>
+        <Paper sx={{width: '100%'}}>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => <>
+                                <TableCell key={column.id} align={column.align} sx={{ minWidth: column.minWidth }}>
+                                    {column.label}
+                                </TableCell>
+                            </>)}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {startAfterIds.map((startAfterId, pageId) => <>
+                            <SubmissionsPage
+                                startAfterId={startAfterId}
+                                startIndex={pageId * rowsPerPage + 1}
+                                columns={columns}
+                                loadSubmissions={loadSubmissions}
+                                onMoreLoaded={(lastId) => onMoreLoaded(pageId, lastId)} />
+                        </>)}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <InfiniteScrollLoading hasMore={hasMore} loadMore={loadNextPage} />
+        </Paper>
+    </>
 }
 
 
 export function UserSubmissionsTable({rowsPerPage, userId}: {rowsPerPage: number, userId: string}) {
-    const onLoadNext = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
+    const loadSubmissions = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
         await onUserSubmissionsChanged(userId, startAfterId ?? null, rowsPerPage, onChange);
 
     const [reset, setReset] = useState(0);
-    const navigate = useNavigate();
-    const {localize} = useContext(LocalizeContext);
     useEffect(() => setReset(r => r + 1), [userId]);
-    return <SubmissionsTable reset={reset} navigate={navigate} onLoadNext={onLoadNext} columns={columns.filter(c => c.id !== 'userDisplayName')} localize={localize}/>
+    return <SubmissionsTable reset={reset} loadSubmissions={loadSubmissions} columns={columns.filter(c => c.id !== 'userDisplayName')} rowsPerPage={rowsPerPage}/>
 }
 
 export function UserExerciseSubmissionsTable({rowsPerPage, userId, courseId, exerciseId}: {
     rowsPerPage: number, userId: string, courseId: string, exerciseId: string
 }) {
-    const onLoadNext = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
+    const loadSubmissions = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
         await onUserExerciseSubmissionsChanged(userId, courseId, exerciseId, 'desc', startAfterId ?? null, rowsPerPage, onChange);
 
     const [reset, setReset] = useState(0);
-    const navigate = useNavigate();
-    const {localize} = useContext(LocalizeContext);
     useEffect(() => setReset(r => r + 1), [userId]);
-    return <SubmissionsTable reset={reset} navigate={navigate} onLoadNext={onLoadNext} columns={columns.filter(c => c.id !== 'userDisplayName' && c.id !== 'courseTitle' && c.id !== 'exerciseTitle')} localize={localize}/>
+    return <SubmissionsTable reset={reset} loadSubmissions={loadSubmissions} columns={columns.filter(c => c.id !== 'userDisplayName' && c.id !== 'courseTitle' && c.id !== 'exerciseTitle')} rowsPerPage={rowsPerPage}/>
 }
 
 export function UserDateSubmissionsTable({rowsPerPage, userId, startDate, endDate}: {rowsPerPage: number, userId: string, startDate: Date, endDate: Date}) {
-    const onLoadNext = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
+    const loadSubmissions = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
         await onUserSubmissionsChanged(userId, startAfterId ?? null, rowsPerPage, onChange, startDate, endDate, 'asc');
 
     const [reset, setReset] = useState(0);
-    const navigate = useNavigate();
-    const {localize} = useContext(LocalizeContext);
     useEffect(() => setReset(r => r + 1), [userId]);
-    return <SubmissionsTable reset={reset} navigate={navigate} onLoadNext={onLoadNext} columns={columns.filter(c => c.id !== 'userDisplayName')} localize={localize}/>
+    return <SubmissionsTable reset={reset} loadSubmissions={loadSubmissions} columns={columns.filter(c => c.id !== 'userDisplayName')} rowsPerPage={rowsPerPage}/>
 }
 
 
 export function CourseSubmissionsTable({rowsPerPage, course}: {rowsPerPage: number, course: Course}) {
-    const onLoadNext = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
+    const loadSubmissions = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
         await onCourseSubmissionsChanged(course.id, startAfterId ?? null, rowsPerPage, onChange);
 
     const [reset, setReset] = useState(0);
-    const navigate = useNavigate();
-    const {localize} = useContext(LocalizeContext);
     useEffect(() => setReset(r => r + 1), [course.id]);
-    return <SubmissionsTable reset={reset} navigate={navigate} onLoadNext={onLoadNext} columns={columns.filter(c => c.id !== 'courseTitle')} localize={localize}/>
+    return <SubmissionsTable reset={reset} loadSubmissions={loadSubmissions} columns={columns.filter(c => c.id !== 'courseTitle')} rowsPerPage={rowsPerPage}/>
 }
 
 export function ExerciseSubmissionsTable({rowsPerPage, course, exercise, mode}: {rowsPerPage: number, course: Course, exercise: Exercise, mode: 'all' | 'best'}) {
-    const onLoadNext = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
+    const loadSubmissions = async (startAfterId: string | null, onChange: (submissions: SubmissionResult[], more: boolean) => void) =>
         await onSubmissionsChanged(course.id, exercise.id, mode, startAfterId ?? null, rowsPerPage, onChange);
 
     const [reset, setReset] = useState(0);
-    const navigate = useNavigate();
-    const {localize} = useContext(LocalizeContext);
     useEffect(() => setReset(r => r + 1), [course.id, exercise.id, mode]);
-    return <SubmissionsTable reset={reset} navigate={navigate} onLoadNext={onLoadNext} columns={columns.filter(c => c.id !== 'courseTitle' && c.id !== 'exerciseTitle')} localize={localize}/>
+    return <SubmissionsTable reset={reset} loadSubmissions={loadSubmissions} columns={columns.filter(c => c.id !== 'courseTitle' && c.id !== 'exerciseTitle')} rowsPerPage={rowsPerPage}/>
 }

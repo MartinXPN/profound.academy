990984a28bb0e4ab137a1aed0a760159a44ceaca
MartinXPN <mirakyanmartin@gmail.com>
2022-06-15 16:15:35 +0400

    Move home with Link from AppBar

--- src/user/ProfileAppBar.tsx
+++ src/user/ProfileAppBar.tsx
@@ -1,21 +1,18 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import AppBarNotifications from "./Notifications";
 import {AppBarProfile} from "./Auth";
 import {Button, SvgIcon, Typography} from "@mui/material";
 import { ReactComponent as Logo } from "../logo.svg";
 
 export default function ProfileAppBar() {
-    const navigate = useNavigate();
-    const onHomeClicked = () => navigate('/');
-
-    return (
+    return <>
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" color="default">
                 <Toolbar>
-                    <Button key="home" color="inherit" onClick={onHomeClicked} size="large" sx={{textTransform: 'none'}}>
+                    <Button color="inherit" size="large" sx={{textTransform: 'none'}} component={Link} to="/">
                         <SvgIcon fontSize="large"><Logo/></SvgIcon>
                         <Typography fontWeight="bold" sx={{ml: 1}}>Profound Academy</Typography>
                     </Button>
@@ -25,5 +22,5 @@ export default function ProfileAppBar() {
                 </Toolbar>
             </AppBar>
         </Box>
-    );
+    </>
 }

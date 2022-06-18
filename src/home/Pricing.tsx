6be0939bd5bd506bee03bc49c55782c07546f9e6
MartinXPN <mirakyanmartin@gmail.com>
2022-06-18 12:10:38 +0400

    Added Pricing plans

--- src/home/Pricing.tsx
+++ src/home/Pricing.tsx
@@ -1,26 +1,57 @@
-import {memo} from "react";
-import {Container, Typography} from "@mui/material";
-import Box from "@mui/material/Box";
+import {memo, ReactNode} from "react";
+import {Button, Card, CardActions, CardContent, Stack, Grid, Typography, Box} from "@mui/material";
+import {Check} from "@mui/icons-material";
+
+function Benefit({name}: {name: string}) {
+    return <>
+        <Stack direction="row" alignItems="center" gap={1}>
+            <Check /><Typography variant="body1">{name}</Typography>
+        </Stack>
+    </>
+}
+
+function Plan({name, price, children}: {name: string, price: string, children: ReactNode}) {
+    return <Grid item>
+        <Card sx={{ width: 240, borderRadius: 8, padding: 4 }}>
+            <CardContent>
+                <Typography variant="h5" textAlign="center">{name}</Typography>
+                <Typography variant="h6" textAlign="center">{price}</Typography>
+                <Box marginBottom={2} />
+
+                {children}
+            </CardContent>
+            <CardActions>
+                <Grid container justifyContent="center">
+                    <Button size="small" color="primary" variant="contained">Get started</Button>
+                </Grid>
+            </CardActions>
+        </Card>
+    </Grid>
+}
 
 function Pricing() {
     return <>
-        <Container>
-            <Typography variant="h5" textAlign="center" marginTop={5}>
-                Picking the best plan for you
-            </Typography>
+        <Typography variant="h1" fontSize={32} textAlign="center" marginTop={10}>
+            Picking the best plan for you
+        </Typography>
 
-            <Box sx={{ my: 2 }}>
-                {[...new Array(32)]
-                    .map(
-                        () => `Cras mattis consectetur purus sit amet fermentum.
-    Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-    Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                    )
-                    .join('\n')}
-            </Box>
+        <Grid container justifyContent="center" alignContent="center" alignItems="center"
+            spacing={4} paddingY={4} marginBottom={10}>
+            {/*Free*/}
+            <Plan name="Free" price="$0/month">
+                <Benefit name="Participate in courses" />
+                <Benefit name="Access side quests" />
+                <Benefit name="Community of learners" />
+            </Plan>
 
-        </Container>
+            {/*Pro*/}
+            <Plan name="Pro" price="$299/month">
+                <Benefit name="Group tutoring" />
+                <Benefit name="Certificates" />
+                <Benefit name="Weekly meetings" />
+                <Benefit name="Personal guidance" />
+            </Plan>
+        </Grid>
     </>
 }
 

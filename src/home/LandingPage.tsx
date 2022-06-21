56e4deba89416e7dd65864707a39a18489bafcce
MartinXPN <mirakyanmartin@gmail.com>
2022-06-21 19:17:05 +0400

    Minor changes in the landing page for SEO

--- src/home/LandingPage.tsx
+++ src/home/LandingPage.tsx
@@ -22,7 +22,7 @@ function Header({onCoursesClicked}: {onCoursesClicked: () => void}) {
                 }
                 description={
                     <Typography variant="h3" color="white" fontSize={18} marginTop={2} marginBottom={4}>
-                        Explore tailored courses that guide your programming journey from beginner to the advanced level.
+                        Explore tailored programming courses that guide your learning journey from beginner to the advanced level.
                     </Typography>
                 }
                 media={<Suspense fallback={<></>}><Product width={largeScreen ? '100%' : 300} height={largeScreen ? '100%' : 300} /></Suspense>}
@@ -48,25 +48,28 @@ function LandingPage({error, onCoursesClicked, onPricingClicked}: {
         {onCoursesClicked && <Header onCoursesClicked={onCoursesClicked} />}
 
         <Feature title="Learn Through Practice"
-                 description="Each concept in the courses is explained through many exercises that help you master the topics.
-                 Everything is hands-on and interactive, so you make progress by solving various challenges instead of only consuming content."
+                 description="Each concept in the courses is explained through many hands-on exercises that help you master the topic.
+                 The curriculum is fully hands-on and interactive, so you make progress by solving various challenges instead of only consuming content."
                  media="/demo/practice.png" mediaPosition="left"/>
 
         <Feature title="Group Tutoring"
                  description="Sign up for group tutoring sessions to get the benefit of a more regular practice.
-                 Meetings take place at the time you pick. You can participate in 2-3 weekly sessions."
+                 Learn programming consistently through weekly meetings and personalized guidance.
+                 Meetings take place at the time you pick, while you have the flexibility to participate in 2-3 weekly sessions."
                  media="/demo/group-tutoring.jpg" mediaPosition="right"
                  action="Sign up for group tutoring" onButtonClicked={onPricingClicked} />
 
         <Feature title="Free Tailored Courses"
                  description="Sign up for courses of different levels and start learning for free!
+                 Study Python, Javascript, and many others by taking a free course.
                  Courses contain exercises of different difficulty levels to keep you engaged and motivated."
                  media="/demo/courses.jpg" mediaPosition="left"
                  action="Explore Courses" onButtonClicked={onCoursesClicked} />
+
         <DiscordInvite />
 
         <Feature title="Profound Academy for Individuals"
-                 description="With Profound Academy students have the flexibility of learning at their own pace.
+                 description="With Profound Academy, students have the flexibility of learning at their own pace.
                  Each concept is explained with a supplementary exercise, where the platform provides instant feedback for each submission.
                  Every exercise can be submitted with a single click, providing instant feedback on the correctness of a solution."
                  media="/demo/individual.jpg" mediaPosition="right"
@@ -79,7 +82,7 @@ function LandingPage({error, onCoursesClicked, onPricingClicked}: {
                  media="/demo/teacher.jpg" mediaPosition="left" />
 
         <Feature title="Profound Academy for Institutions"
-                 description="Institutions can get the courses and competitions created by us to teach their students without creating everything from scratch.
+                 description="Institutions can get the courses and competitions created by us to teach their students without creating every course from scratch.
                  All the content is customizable, so the tutors can adjust the courses to their needs.
                  Please contact us so that we can best help you get started."
                  media="/demo/institution.jpg" mediaPosition="right" />
